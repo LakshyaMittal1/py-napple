be14655f401ea0e31cf5c11e88b9a31e509cf952
LakshyaMittal1 <lakshyamittal23503@gmail.com>
2026-02-14 11:49:52 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,153 @@
+from fastapi import FastAPI, Request, HTTPException, status
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+# from fastapi.responses import HTMLResponse
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+from schemas import PostCreate, PostResponse
+
+
+app = FastAPI()
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates") # telling fastapi where the templates are
+
+posts: list[dict] = [
+    {
+        'id': 1,
+        'author': 'Lakshya Mittal',
+        'title': 'FastAPI is Awesome',
+        'content': 'This framework is really easy to use and super fast',
+        'date_posted': 'April 20, 2025'
+    },
+    {
+        'id': 2,
+        'author': 'Darshan Mittal',
+        'title': 'Python is great for web development.',
+        'content': 'Python is a great language for web dev, and FastAPI makes it even better.',
+        'date_posted': 'April 21, 2025'
+    },
+]
+
+
+# but we can let two or more urls to have same page by using and stacking them using decorators
+
+# include_in_schema=False lets the route/url to be hidden in the docs
+
+""" @app.get('/posts', response_class=HTMLResponse, include_in_schema=False)
+@app.get('/', response_class=HTMLResponse, include_in_schema=False)
+def home(): # A regualar synchronous function
+    # return ({"message": "Welcome to the Home Page."}) # JSON
+    return f"<h1>{posts[0]['title']}</h1>" """
+
+
+@app.get('/posts', include_in_schema=False, name='posts')
+@app.get('/', include_in_schema=False, name='home')
+def home(request: Request):
+    return templates.TemplateResponse(
+        request, 
+        "home.html", 
+        {"posts": posts, "title": "Home"}
+    )
+
+@app.get('/posts/{post_id}', include_in_schema=False, name='post_page')
+def post_page(request: Request, post_id: int):
+    for post in posts:
+        if post.get('id') == post_id:
+            title = str(post['title'][:50]) + '...'
+            return templates.TemplateResponse(
+                request,
+                'post.html',
+                {'post': post, 'title': title}
+            )
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found!")
+
+
+""" in case of duplicate urls or paths, the first one is registered by fastapi and gets considered
+@app.get('/api/posts')
+def get_posts():
+    return "hello" # converts the list of dictionary/dictionaries to JSON automatically
+ """
+
+@app.get('/api/posts', response_model=list[PostResponse])
+def get_posts():
+    return posts # converts the list of dictionary/dictionaries to JSON automatically
+
+@app.post(
+        "/api/posts",
+        response_model=PostResponse,
+        status_code=status.HTTP_201_CREATED
+)
+def create_post(post: PostCreate):
+    new_id = max(p['id'] for p in posts) + 1 if posts else 1
+    new_post = {
+        'id': new_id,
+        'author': post.author,
+        'title': post.title,
+        'content': post.content,
+        'date_posted': "April 23, 2025",
+    }
+    posts.append(new_post)
+
+    return new_post
+
+
+@app.get('/api/posts/{post_id}', response_model=PostResponse)
+def get_post(post_id: int):
+    for post in posts:
+        if post.get('id') == post_id:
+            return post
+    # return {"error": "Post not found!"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found!")
+
+
+
+
+## StarletteHTTPException Handler
+@app.exception_handler(StarletteHTTPException)
+def general_http_exception_handler(request: Request, exception: StarletteHTTPException):
+    message = (
+        exception.detail
+        if exception.detail
+        else "An error occurred. Please check your request and try again."
+    )
+
+    if request.url.path.startswith("/api"):
+        return JSONResponse(
+            status_code=exception.status_code,
+            content={"detail": message}
+        )
+    return templates.TemplateResponse(
+        request,
+        "error.html",
+        {
+            "status_code": exception.status_code,
+            "title": exception.status_code,
+            "message": message,
+        },
+        status_code=exception.status_code,
+    )
+
+
+## RequestValidationError Handler
+@app.exception_handler(RequestValidationError)
+def validation_exception_handler(request: Request, exception: RequestValidationError):
+    if request.url.path.startswith('/api'):
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+            content={"detail": exception.errors()},
+        )
+    return templates.TemplateResponse(
+        request,
+        "error.html",
+        {
+            'status_code': status.HTTP_422_UNPROCESSABLE_CONTENT,
+            'title': status.HTTP_422_UNPROCESSABLE_CONTENT,
+            "message": "Invalid request. Please check your input and try again."
+        },
+        status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+    )
